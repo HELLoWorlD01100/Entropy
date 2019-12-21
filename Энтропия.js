7f6eaa52f885a7a6c85058640ae1e2c708bf7b7d
HELLoWorlD01100 <54909098+HELLoWorlD01100@users.noreply.github.com>
2019-12-21 22:21:49 +0500

    Update Энтропия.js

--- Энтропия.js
+++ Энтропия.js
@@ -1,22 +1,20 @@
-function GetBaseLog (base, value) {
-  return Math.log(value) / Math.log(base);
+function GetBaseLog(base, value) {
+    return Math.log(value) / Math.log(base);
 }
 let string = "!!!!!!!!22";
 let alphabet = [];
 let number = 0;
 let entropy = 0;
 for (let i = 0; i < string.length; i++)
-	alphabet[string.charAt(i)] = 0;
+    alphabet[string.charAt(i)] = 0;
 for (let i = 0; i < string.length; i++)
-	console.log(alphabet[string.charAt(i)]++);//количество
-for (let i in alphabet)
-{
- 		alphabet[i] = alphabet[i] / string.length;
- 		number++;
+    console.log(alphabet[string.charAt(i)]++);
+for (let i in alphabet) {
+    alphabet[i] = alphabet[i] / string.length;
+    number++;
 }
-if (number != 1)
-{
-	for (let i in alphabet) 
- 		entropy += -(alphabet[i] * GetBaseLog(number, alphabet[i]));
+if (number != 1) {
+    for (let i in alphabet)
+        entropy += -(alphabet[i] * GetBaseLog(number, alphabet[i]));
 }
 console.log(entropy);
